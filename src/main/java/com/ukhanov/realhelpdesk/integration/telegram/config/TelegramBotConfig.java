6d23432c68d37a2e2bf89388ee6f228d20dff71a
r2d2 <denis.hanov@gmail.com>
2025-07-14 18:30:09 +0300

    update: pom

--- src/main/java/com/ukhanov/realhelpdesk/integration/telegram/config/TelegramBotConfig.java
+++ src/main/java/com/ukhanov/realhelpdesk/integration/telegram/config/TelegramBotConfig.java
@@ -1,13 +1,18 @@
 package com.ukhanov.realhelpdesk.integration.telegram.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+@Component
 @Configuration
 @ConditionalOnProperty(name = "telegram.enabled", havingValue = "true", matchIfMissing = false)
+@ConfigurationProperties(prefix = "telegram")
 @ComponentScan(basePackages = "com.ukhanov.realhelpdesk.integration.telegram")
 public class TelegramBotConfig {
+
   private String token;
   private String username;
   private String domain;
@@ -20,19 +25,19 @@ public class TelegramBotConfig {
     return domain;
   }
 
-  public void setDomain(String domain) {
-    this.domain = domain;
+  public String getUsername() {
+    return username;
   }
 
   public void setToken(String token) {
     this.token = token;
   }
 
-  public String getUsername() {
-    return username;
-  }
-
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public void setDomain(String domain) {
+    this.domain = domain;
   }
 }
